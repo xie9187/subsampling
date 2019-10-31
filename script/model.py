ee80abe0be9013443db86a51331b5b13dd7fd0fe
Linhai Xie <xielinhai@gmail.com>
2019-10-31 21:48:00 +0000

    normal sampling

--- script/model.py
+++ script/model.py
@@ -108,10 +108,9 @@ def my_sampling(features, t, k=100, noise_flag=True):
     b, n, d = features.get_shape().as_list() # b, n, d
     # score for each point
     score_h1 = model_utils.dense_layer(tf.reshape(features, [-1, d]), 256, 'score_h1') # b*n, 256
-    score = model_utils.dense_layer(features, 1, 'score', activation=tf.nn.sigmoid) # b*n, 1
+    score = model_utils.dense_layer(score_h1, 1, 'score', activation=tf.nn.sigmoid) # b*n, 1
     if noise_flag:
         noise = tf.nn.relu(tf.random.truncated_normal([b*n, 1], stddev=t**2)) # b*n, 1
-        print score, noise
         score = tf.reshape((score + noise)/2, [-1, n]) # b, n
     else:
         score = tf.reshape(score, [-1, n]) # b, n
