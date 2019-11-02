fd675846c5762cc79e1ef70a40b6ea4a3c1f7037
Linhai Xie <xielinhai@gmail.com>
2019-11-02 00:03:09 +0000

    simple normal

--- script/model.py
+++ script/model.py
@@ -27,7 +27,7 @@ class pointnet(object):
         else:
             self.sampled_input = self.input
 
-        logits = self.get_model(sampled_input, self.is_training)
+        logits = self.get_model(self.sampled_input, self.is_training)
         y = tf.argmax(logits, axis=1, output_type=tf.int32)
         self.loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.label, logits=logits)
         self.opt = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
