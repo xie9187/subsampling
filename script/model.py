0b34dbdf27e4657580cfd47a6c294de8734c6eae
Linhai Xie <xielinhai@gmail.com>
2019-11-02 17:17:18 +0000

    keep shape invariant

--- script/model.py
+++ script/model.py
@@ -129,7 +129,7 @@ def my_sampling(features, t, k=100, noise_flag=True):
 
     # sampled features
     # top_scores = tf.tile(tf.expand_dims(top_scores, axis=2), [1, 1, d]) # b, k, d
-    top_scores = tf.tile(tf.expand_dims(tf.readuce_mean(top_scores, axis=1, keepdims=True), axis=2), [1, k, d]) # b, k, d
+    top_scores = tf.tile(tf.expand_dims(tf.reduce_mean(top_scores, axis=1, keepdims=True), axis=2), [1, k, d]) # b, k, d
     sub_features = tf.pow(top_scores, t) * top_features
     return sub_features, sorted_features, sorted_score
 
